6af433e4e646652482fbe33d8f89005135bc0f76
Lakshay Tomar <lakshay.tomar2002@gmail.com>
2024-04-03 22:31:03 +0530

    update configurations

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   return mongoose
-    .connect(process.env.MONGODB_URI)
+    .connect('mongodb://localhost:27017/cms_db')
     .then(() => console.log(`connection to database established...`))
     .catch((err) => console.log(err));
 };
